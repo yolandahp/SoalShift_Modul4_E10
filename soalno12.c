8921248c25109d618f8f3a3e73594e506b9003c7
Unknown <yolandahertita903@gmail.com>
2017-11-15 18:50:19 +0700

    baru modif xmpreaddir dan xmpgetarr

--- soalno12.c
+++ soalno12.c
@@ -1,119 +1,106 @@
-/*
-cara run program sama seperti di modul, pastikan format direktori sudah benar
 
-cara kerjanya:
-program akan menjalankan fungsi xmp_readdir
-*/
 #define FUSE_USE_VERSION 28
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
 
-//alamat file system yang akan di mount
-static const char *dirpath = "/home/titut/Documents";
+#ifdef HAVE_SETXATTR
+#include <sys/xattr.h>
+#endif
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef linux
+#define _XOPEN_SOURCE 700
+#endif
+
+static const char *dirpath = "/home/titut/Documents"; //direktori yang dimount
 
 //fungsi untuk mengambil atribut. fungsi ini akan dipanggil setiap fungsi xmp readdir membaca isi direktori 
-static int xmp_getattr(const char *path, struct stat *stbuf)
-{
+static int xmp_getattr(const char *path, struct stat *stbuf){
   int res;
   char fpath[1000];
-  char newFile[100];
-  int l = strlen(path);
-  printf("path   : %s, len: %d\n", path, l);
-  //---------------------------------------------------------
-  //ini untuk membaca nama file aslinya (tanpa ekstensi .bak)
-  //dia akan memodifikasi isi dari path(yang sebelumnya ada .bak nya) jadi nama asli file (tidak ada .bak nya) supaya bisa di get attribute
-  if (strcmp(path, "/") != 0) {
-    memcpy(newFile, path, strlen(path) - 4);
-    newFile[strlen(path) - 4] = '\0';
-  } else {
-    memcpy(newFile, path, strlen(path));
-  }
-  //---------------------------------------------------------
-  printf("newFile: %s\n", newFile);
-  sprintf(fpath,"%s%s",dirpath, newFile);
+  
+  sprintf(fpath,"%s%s",dirpath,path);
+
   res = lstat(fpath, stbuf);
 
-  if (res == -1)
-    return -errno;
-
+  if(res == -1) return -errno;
   return 0;
 }
 
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
-{
- char fpath[1000];
-  if(strcmp(path,"/") == 0)
-  {
+//membaca direktori
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi){
+  DIR *dp;
+  struct dirent *de;
+  char fpath[1000];
+
+  if(strcmp(path,"/") == 0){
     path=dirpath;
     sprintf(fpath,"%s",path);
   }
   else sprintf(fpath, "%s%s",dirpath,path);
-  int res = 0;
-
-  DIR *dp;
-  struct dirent *de;
 
   (void) offset;
   (void) fi;
 
   dp = opendir(fpath);
-  if (dp == NULL)
-    return -errno;
+  if (dp == NULL) return -errno;
 
   while ((de = readdir(dp)) != NULL) {
-    char *newName;
-    //ini buat menambahkan .bak di path nya
-    newName = strcat(de->d_name, ".bak");
     struct stat st;
     memset(&st, 0, sizeof(st));
     st.st_ino = de->d_ino;
     st.st_mode = de->d_type << 12;
-    res = (filler(buf, newName, &st, 0));
-      if(res!=0) break;
+    if (filler(buf, de->d_name, &st, 0)) break;
   }
 
   closedir(dp);
   return 0;
 }
 
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
- 	   struct fuse_file_info *fi)
-{
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
   char fpath[1000];
-  char newFile[100];
-  //sama seperti di get atrribute, cuma ini buat bisa untuk di read berdasarkan nama aslinya
-  if(strcmp(path,"/") == 0)
-  {
-    memcpy(newFile, path, strlen(path));
-    path=dirpath;
-    sprintf(fpath,"%s",newFile);
-  }
-  else {
-    memcpy(newFile, path, strlen(path) - 4);
-    newFile[strlen(path) - 4] = '\0';
 
+  if(strcmp(path,"/") == 0){
+    path=dirpath;
+    sprintf(fpath,"%s", path);
+  }
+  else{
     sprintf(fpath, "%s%s",dirpath,newFile);
   }
+
   int res = 0;
-  int fd = 0 ;
+  int fd = 0;
 
-  (void) fi;
-  fd = open(fpath, O_RDONLY);
-  if (fd == -1)
-    return -errno;
+  char ext[4]; //extensionnya
+  int i, l = strlen(fpath); //lalala.jpg
+  for(i = 4; i >= 1; i++){
+    ext[4-i] = fpath[l-i];
+  }
 
-  res = pread(fd, buf, size, offset);
-  if (res == -1)
-    res = -errno;
+  if(!strcmp(temp, ".doc") || !strcmp(temp, ".txt") || !strcmp(temp, ".pdf")){
+    
+  }
+  else{
+    (void) fi;
+    fd = open(fpath, O_RDONLY);
+    if (fd == -1) return -errno;
 
-  close(fd);
-  return res;
+    res = pread(fd, buf, size, offset);
+    if (res == -1) res = -errno;
+
+    close(fd);
+    return res;
+  }
 }
 
 static struct fuse_operations xmp_oper = {
